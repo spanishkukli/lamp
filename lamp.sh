9532a47e09a9bb388605db09fed91e95832aeb89
kukli <72162161+spanishkukli@users.noreply.github.com>
2023-06-02 10:27:27 +0200

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Install packages
-apt update
-apt install gnupg2 ufw apache2 mariadb-server -y
+apt update && apt install gnupg2 ufw apache2 mariadb-server -y
 apt-get install ca-certificates apt-transport-https software-properties-common -y
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
 wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
