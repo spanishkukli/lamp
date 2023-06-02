4ed2ac7c720b158f5c05b321b90f6f016326edc3
kukli <72162161+spanishkukli@users.noreply.github.com>
2023-06-02 10:24:17 +0200

    Add files via upload

---
+++ lamp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install packages
+apt update
+apt install gnupg2 ufw apache2 mariadb-server -y
+apt-get install ca-certificates apt-transport-https software-properties-common -y
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
+wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
+apt-get update -y
+apt-get install php8.0
+
+# Enable HTTP y HTTPS
+ufw app info "WWW Full"
+ufw allow in "WWW Full"
+
+# mysql_secure_installation 
+read -p "Execute mysql_secure_installation? (recomended) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+   mysql_secure_installation 
+fi
+
